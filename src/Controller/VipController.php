4456b89963a1f8d99cbd835eb6a89d4f4d1ebe87
Mhyssa <jolym8@gmail.com>
2024-06-05 15:37:14 +0200

    :sparkles: VIP VOTER

--- src/Controller/VipController.php
+++ src/Controller/VipController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VipController extends AbstractController
 {
     #[Route('/vip', name: 'app_vip')]
+    #[IsGranted('VOTER_VIP')]
     public function index(): Response
     {
         return $this->render('vip/index.html.twig', [
